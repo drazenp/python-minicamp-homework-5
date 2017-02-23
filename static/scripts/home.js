1b1ba10f742e353bf0d002b63ca948687aaacb16
drazenp <drazenp86@gmail.com>
2017-02-23 05:10:46 +0100

    Finished task 5.

---
+++ static/scripts/home.js
@@ -0,0 +1,17 @@
+function validateForm() {
+	var name = document.getElementById('nameInput').value;
+	var species = document.getElementById('speciesInput').value;
+	var age = document.getElementById("ageInput").value;
+	
+	if (!name.length || !species.length || !age.length) {
+		alert("Fieds must not be blank.");
+		return false;
+	}
+	
+	if (isNaN(parseInt(age)) || typeof parseInt(age) !== 'number') {
+		alert('Age must be a number.');
+		return false;
+	}
+	
+	return true;
+};
